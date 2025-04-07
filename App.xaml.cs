9c95ea7962e8b32a4f45d20c5e77d68429fbd4db
Dianne Cruz <diannecruz0404@gmail.com>
2025-04-07 14:44:17 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BookHaven
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
